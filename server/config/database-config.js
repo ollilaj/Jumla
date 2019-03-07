17e71e3f96ce815aaf5c62e2ac8e780b3fe85c62
Jacob Ollila <jake.ollila@clearsurance.com>
2019-03-06 22:00:38 -0500

    Added mongodb url for prod

--- server/config/database-config.js
+++ server/config/database-config.js
@@ -1,4 +1,7 @@
+let username = process.env.MONGO_USERNAME;
+let password = process.env.MONGO_PASSWORD;
+
 module.exports = {
-	url: 'mongodb://localhost:27017/Jumla',
-	secret: 'my_secret'
+	url: `mongodb://${username}:${password}@docdb-jumla.cluster-cahuszcb591a.us-east-1.docdb.amazonaws.com:27017/?ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0`,
+	urlLocal: `mongodb://localhost:27017/Jumla`
 };
